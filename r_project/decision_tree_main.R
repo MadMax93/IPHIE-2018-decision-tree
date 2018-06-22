0fe00bc83d11aa19932fa6d6e93ed7b30ba990d1
MadMax93 <m.kurscheidt93@gmail.com>
2018-06-22 11:54:45 +0200

    refactoring filenames and restructuring folders!

--- r_project/decision_tree_main.R
+++ r_project/decision_tree_main.R
@@ -1,2 +1,16 @@
+df <- read.csv(file="./IPHIE-2018-decision-tree/dataset_diabetes/dataset_diabetes/diabetic_data.csv", header=TRUE, sep=",")
 
-MyData <- read.csv(file="../dataset_diabetes/dataset_diabetes/dataset_diabetes.csv", header=TRUE, sep=",")
+head(df)
+
+library(party)
+library(rpart)
+library(rpart.plot)
+library(readr)
+library(dplyr)
+set.seed(100)
+
+df$readmitted = factor(df$readmitted)
+
+rtree_fit <- rpart(readmitted ~ ., data = df) 
+
+rpart.plot(rtree_fit)
